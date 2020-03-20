f4c5803c16d70fc294fca45e7a8077b30933e939
murad <jamburia.muradi@mail.ru>
2020-03-20 15:43:12 +0400

    Update camera.js

--- scripts/camera.js
+++ scripts/camera.js
@@ -1,8 +1,6 @@
 
   let video = document.createElement('video');
-  let button = document.createElement('button');
-  button.className = 'both';
-  button.textContent = 'test';
+  
   
   video.className = 'videoElement';
   video.autoplay = true;
@@ -19,13 +17,9 @@
     .catch((err) => {
       console.log(err);
     });
-    document.body.appendChild(button)
+    
     document.body.appendChild(video);
-    button.addEventListener("click", function(){
-    localStream.getTracks().forEach((track) => {
-    track.stop();
-  })
-});
+   
    
     
      
